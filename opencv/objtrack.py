2f579ea7c19936a9a8256e03f6965484aebe1518
rayxyz <625524105@qq.com>
2018-05-09 14:34:59 +0800

    pass vid obj tracking

--- opencv/objtrack.py
+++ opencv/objtrack.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import time
 
 cap = cv.VideoCapture('/home/ray/file/cv/news.mp4')
 
@@ -7,7 +8,7 @@ ret, frame = cap.read()
 
 # print(frame)
 
-r, h, c, w = 300, 400, 100, 500
+r, h, c, w =  250,90,200,125
 track_window = (r, h, c, w)
 
 roi = frame[r:r+h, c:c+w]
@@ -19,8 +20,8 @@ cv.normalize(roi_hist, roi_hist, 0, 255, cv.NORM_MINMAX)
 term_crit = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)
 
 while True:
+    time.sleep(0.03)
     ret, frame = cap.read()
-    # cv.imshow('frame', frame)
 
     if ret == True:
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
@@ -29,8 +30,12 @@ while True:
         ret, track_window = cv.meanShift(dst, track_window, term_crit)
 
         x, y, w, h = track_window
-        img2 = cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        cv.imshow('img2', img2)
+        print(x,y,w,h)
+        img = cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv.imshow('img', img)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cv.waitKey(0)
 cv.destroyAllWindows()
